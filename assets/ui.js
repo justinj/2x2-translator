35d4d1f149d28d2b832435036629277410aa0552
Justin Jaffray <justin.jaffray@gmail.com>
2012-10-02 13:58:55 -0400

    moved js out of index.html

---
+++ assets/ui.js
@@ -0,0 +1,29 @@
+$(function(){
+  $("#go").click(run);
+});
+function run() 
+{
+  box = $("#alg_box")
+    results = $("#results");
+  var inputs = box.val().split("\n")
+    results.html("");
+  $.each(inputs, function(index, input) {
+    var algs = Translator.translate(Translator.formatInput(input, getOptions()), getOptions());
+    algs = algs.map(function(input) {
+      return "<a class=\"alglink\">"+input+"</a>";
+    });
+    results.append(algs.join("<BR>") + "<BR><BR>")
+  });
+  $(".alglink").click(function(event) {
+    $("#notes").val($("#notes").val()+event.target.innerHTML+"\n");
+  });
+}
+
+function getOptions()
+{
+  options = {}
+  options["remove_trailing_u"] = $("#remove_trailing_u")[0].checked
+    options["one_d"] = $("#one_d")[0].checked
+    options["show_y_rotations"] = $("#show_y_rotations")[0].checked
+    return options;
+}
